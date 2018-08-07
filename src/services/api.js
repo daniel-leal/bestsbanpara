d4972d14faf4e20914f8e8dd5a67510d4c339763
Daniel Leal <danielleal94@gmail.com>
2018-08-07 10:24:48 -0300

    Remove old api comments; Export api and apiAuth

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-/* Endereços para cada emulador/simulador:
-** Genymotion:              http://10.0.3.2:3333/
-** Emulador Android Studio: http://10.0.2.2:3333/
-** Simulador IOS:           http://localhost:3333/
-*/
 const api = axios.create({
   baseURL: 'https://api.cartolafc.globo.com/',
 });
@@ -13,4 +8,4 @@ const apiAuth = axios.create({
   baseURL: 'https://login.globo.com/',
 });
 
-export default apiAuth
+export {api, apiAuth};
